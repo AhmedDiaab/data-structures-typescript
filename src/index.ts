8cd6d557f6f883dc243877b946df03e20f1bf69f
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2025-03-04 14:11:57 +0200

    feat(): add push method to linked list

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { LinkedList } from "./linked-list";
 
 const ll = new LinkedList(4)
+ll.push(3)
+ll.push(2)
 console.log(ll);
+console.log('hello world')
--- src/linked-list/linkedList.ts
+++ src/linked-list/linkedList.ts
@@ -1,6 +1,6 @@
 class Node {
-    private value: number;
-    private next: Node | null;
+    value: number;
+    next: Node | null;
 
     constructor(value: number) {
         this.value = value;
@@ -14,8 +14,22 @@ export class LinkedList {
     private length: number;
 
     constructor(value: number) {
-        this.head = new Node(value);
-        this.tail = this.head;
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
         this.length = 1;
+    }
+    
+    push(value: number) {
+        const newNode = new Node(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
     }
 }
